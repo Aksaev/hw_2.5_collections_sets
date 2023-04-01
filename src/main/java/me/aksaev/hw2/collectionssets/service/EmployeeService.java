92a681b768a0e32526779ee090fda1753d7fbdda
Aksaev <mail-ax@ya.ru>
2023-04-01 09:53:10 +0400

    ДЗ 2.5. Коллекции и сеты. Выполнил

---
+++ src/main/java/me/aksaev/hw2/collectionssets/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw2.collectionssets.service;
+
+import me.aksaev.hw2.collectionssets.model.Employee;
+
+import java.util.Collection;
+
+public interface EmployeeService {
+    Employee addEmployee(String firstName, String lastName);
+    Employee removeEmployee(String firstName, String lastName);
+    Employee findEmployee(String firstName, String lastName);
+
+    Collection<Employee> getEmployee();
+}
